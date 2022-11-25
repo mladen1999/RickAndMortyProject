eae33011d329886f8370e41f5a4fce28c0f077a9
Mladen <mladenvts@gmail.com>
2022-11-25 16:10:07 +0100

    Add radio buttons

--- src/components/Characters/Characters.tsx
+++ src/components/Characters/Characters.tsx
@@ -18,14 +18,15 @@ const Characters = () => {
 
         <div className="flex items-center mr-4">
           <input
-            id="inline-radio"
+            checked
+            id="inline-checked-radio"
             type="radio"
             value=""
             name="inline-radio-group"
             className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
           />
           <label
-            htmlFor="inline-radio"
+            htmlFor="inline-checked-radio"
             className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
           >
             Any
@@ -33,17 +34,18 @@ const Characters = () => {
         </div>
         <div className="flex items-center mr-4">
           <input
-            id="inline-2-radio"
+            checked
+            id="inline-checked-radio"
             type="radio"
             value=""
             name="inline-radio-group"
             className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
           />
           <label
-            htmlFor="inline-2-radio"
+            htmlFor="inline-checked-radio"
             className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
           >
-            Alive
+            Live
           </label>
         </div>
         <div className="flex items-center mr-4">
