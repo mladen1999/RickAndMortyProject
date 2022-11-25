74c70a7eb2fe20fb526cb4bee48953918a98a8f8
Mladen <mladenvts@gmail.com>
2022-11-25 16:18:28 +0100

    Fix Card Component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,6 @@
 const Card = ({ name, image }: any) => {
   return (
-    <div className="max-w-sm rounded-lg overflow-hidden h-25 w-25 ml-3 mr-3 round-10">
+    <div className=" rounded-lg overflow-hidden h-30 w-30 ml-3 mr-3 round-10 pt-5">
       <img className="w-full" src={image} alt={name} />
       <div className="font-bold text-xl mb-2 text-center pt-5 pb-10">
         {name}
